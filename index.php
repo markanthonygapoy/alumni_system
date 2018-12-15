d7a50e5f98694963ba8f136e2d20d0207f92789e
Mark Gapoy <markanthonygapoy@gmail.com>
2018-12-15 14:05:46 +0800

    change reCaptcha

--- index.php
+++ index.php
@@ -23,13 +23,7 @@ session_start();
     <input type="text" name="idNumber" id="idNumber" class="form-control" placeholder="ID Number" required autofocus>
     <label for="birthDate" class="sr-only">Birthdate</label>
     <input type="date" name="birthDate" id="birthDate" class="form-control" placeholder="Birthdate" name="bday" min="1000-01-01" max="3000-12-31" class="form-control" required>
-    
-    <div class="form-group">
-                            <div class="g-recaptcha" data-sitekey="6Lf13IEUAAAAAKhxq5opFsX0Gbu-E65kd1If3iKa" data-callback="verifyRecaptchaCallback" data-expired-callback="expiredRecaptchaCallback"></div>
-                            <input class="form-control" data-recaptcha="true" required data-error="Please complete the Captcha">
-                            <div class="help-block with-errors"></div>
-                        </div>
-
+    <div class="g-recaptcha" data-sitekey="6Lf13IEUAAAAAKhxq5opFsX0Gbu-E65kd1If3iKa"></div>
     <div class="form-group">
     <button class="btn btn-lg btn-primary btn-block" name="signIn" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
